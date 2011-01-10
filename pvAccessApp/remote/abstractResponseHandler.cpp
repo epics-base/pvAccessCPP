d7852fa666f60ec1bb72d99ca2b72437f9e0b1b2
miha_vitorovic <unknown>
2011-01-10 14:11:27 +0100

    Moved AbstractResponseHandler definition to a more logical place.

---
+++ pvAccessApp/remote/abstractResponseHandler.cpp
@@ -0,0 +1,42 @@
+/*
+ * abstractResponseHandler.cpp
+ *
+ *  Created on: Jan 10, 2011
+ *      Author: Miha Vitorovic
+ */
+
+#include "remote.h"
+#include "hexDump.h"
+
+#include <byteBuffer.h>
+
+#include <osiSock.h>
+
+#include <sstream>
+
+using std::ostringstream;
+using std::hex;
+
+using namespace epics::pvData;
+
+namespace epics {
+    namespace pvAccess {
+
+        void AbstractResponseHandler::handleResponse(osiSockAddr* responseFrom,
+                Transport* transport, int8 version, int8 command,
+                int payloadSize, ByteBuffer* payloadBuffer) {
+            if(_debug) {
+                char ipAddrStr[48];
+                ipAddrToA(&responseFrom->ia, ipAddrStr, sizeof(ipAddrStr));
+
+                ostringstream prologue;
+                prologue<<"Message [0x"<<hex<<(int)command<<", v0x"<<hex;
+                prologue<<(int)version<<"] received from "<<ipAddrStr;
+
+                hexDump(prologue.str(), _description,
+                        (const int8*)payloadBuffer->getArray(),
+                        payloadBuffer->getPosition(), payloadSize);
+            }
+        }
+    }
+}
