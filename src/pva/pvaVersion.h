7cc0ca9cb1489e12377c31c360224bd1dcb5b1b7
Matej Sekoranja <unknown>
2014-10-13 23:22:09 +0200

    win32 getpid

--- src/pva/pvaVersion.h
+++ src/pva/pvaVersion.h
@@ -25,7 +25,7 @@
 // module version
 // TODO to be generated, etc.
 #define EPICS_PVA_MAJOR_VERSION 4
-#define EPICS_PVA_MINOR_VERSION 0
+#define EPICS_PVA_MINOR_VERSION 1 
 #define EPICS_PVA_MAINTENANCE_VERSION 0
 #define EPICS_PVA_DEVELOPMENT_FLAG 1
 
