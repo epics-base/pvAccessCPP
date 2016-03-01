19031af095003f1d7c2d9e7ee38a5eaaa98ec427
Matej Sekoranja <matej.sekoranja@cosylab.com>
2016-03-01 08:22:05 +0100

    added testClientFactory test

---
+++ testApp/remote/testClientFactory.cpp
@@ -0,0 +1,15 @@
+#include <epicsExit.h>
+#include <pv/clientFactory.h>
+#include <pv/pvAccess.h>
+
+int main()
+{
+    epics::pvAccess::ClientFactory::start();
+    epics::pvAccess::getChannelProviderRegistry()->getProvider("pva");
+    epics::pvAccess::ClientFactory::stop();
+    
+    //epicsThreadSleep ( 3.0 ); 
+    //epicsExitCallAtExits();
+    
+    return 0;
+}
