01dce98a6c4f3c13b2da9daea0af3793a9522dbe
Andrew Johnson <anj@aps.anl.gov>
2016-04-05 18:48:47 -0500

    Add copyright notice

--- src/mb/pv/pvAccessMB.h
+++ src/mb/pv/pvAccessMB.h
@@ -1,3 +1,9 @@
+/**
+ * Copyright - See the COPYRIGHT that is included with this distribution.
+ * pvAccessCPP is distributed subject to a Software License Agreement found
+ * in file LICENSE that is included with this distribution.
+ */
+
 #ifndef _PVACCESSMB_H_
 #define _PVACCESSMB_H_
 
