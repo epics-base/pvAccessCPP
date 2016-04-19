a9e3f274c0242719fdb231562e50925f897c4465
mrkraimer <mrkraimer@comcast.net>
2016-04-19 15:10:41 -0400

    remove dead code

--- src/ioc/PVAServerRegister.cpp
+++ src/ioc/PVAServerRegister.cpp
@@ -37,62 +37,6 @@ using std::endl;
 using namespace epics::pvData;
 using namespace epics::pvAccess;
 
-//class PVAServerCTX;
-//typedef std::tr1::shared_ptr<PVAServerCTX> PVAServerCTXPtr;
-//
-//class PVAServerCTX :
-//    public std::tr1::enable_shared_from_this<PVAServerCTX>
-//{
-//public:
-//    POINTER_DEFINITIONS(PVAServerCTX);
-//    static PVAServerCTXPtr getPVAServerCTX();
-//    void start(std::string const & providerNames);
-//    void stop();
-//    virtual ~PVAServerCTX() {}
-//private:
-//    PVAServerCTX() {}
-//    shared_pointer getPtrSelf()
-//    {
-//        return shared_from_this();
-//    }
-//    ServerContext::shared_pointer ctx;
-//};
-//
-//void PVAServerCTX::start(std::string const & providerNames)
-//{
-//    if(ctx.get()) {
-//        cout<< "PVAServer already started" << endl;
-//        return;
-//    }
-//    ctx = startPVAServer(providerNames,0,true,true);
-//}
-//
-//void PVAServerCTX::stop()
-//{
-//    if(!ctx.get()) {
-//        cout<< "PVAServer already stopped" << endl;
-//        return;
-//    }
-//    ctx->destroy();
-//    ctx.reset();
-//}
-//
-//PVAServerCTXPtr PVAServerCTX::getPVAServerCTX()
-//{
-//    static PVAServerCTXPtr pvPVAServerCTX;
-//    static Mutex mutex;
-//    Lock xx(mutex);
-//
-//    if(!pvPVAServerCTX.get()) {
-//        pvPVAServerCTX = PVAServerCTXPtr(new PVAServerCTX());
-//    }
-//    return pvPVAServerCTX;
-//}
-
-//static void pvaServerExitHandler(void* /*pPrivate*/) {
-//    PVAServerCTX::getPVAServerCTX()->stop();
-//}
-
 static const iocshArg startPVAServerArg0 = { "providerNames", iocshArgString };
 static const iocshArg *startPVAServerArgs[] = {
     &startPVAServerArg0};
@@ -105,13 +49,10 @@ static void startPVAServer(const iocshArgBuf *args)
     char *names = args[0].sval;
     if(!names) {
        startPVAServer(PVACCESS_ALL_PROVIDERS,0,true,true);
-//        PVAServerCTX::getPVAServerCTX()->start(PVACCESS_ALL_PROVIDERS);
     } else {
         std::string providerNames(names);
         startPVAServer(providerNames,0,true,true);
-//        PVAServerCTX::getPVAServerCTX()->start(providerNames);
     }
-//    epicsAtExit(pvaServerExitHandler, NULL);
 }
 
 
