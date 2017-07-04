96272c2a3079ef1ae6687efc2eccd751e74b730c
Michael Davidsaver <mdavidsaver@gmail.com>
2017-07-04 18:02:42 +0200

    add copyright boilerplate

--- examples/spamme.cpp
+++ examples/spamme.cpp
@@ -1,3 +1,8 @@
+/**
+ * Copyright - See the COPYRIGHT that is included with this distribution.
+ * pvAccessCPP is distributed subject to a Software License Agreement found
+ * in file LICENSE that is included with this distribution.
+ */
 #include <deque>
 #include <set>
 #include <vector>
