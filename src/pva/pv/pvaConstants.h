4c873d4905af64de43da60ef26144271af320943
Dirk Zimoch <dirk.zimoch@psi.ch>
2025-11-05 13:06:32 +0100

    Fix MSVC warning C4309
    truncation of constant value
    
    The value 0xFFFF does not fit a signed 16 bit integer.
    Use -1 instead.

--- src/pva/pv/pvaConstants.h
+++ src/pva/pv/pvaConstants.h
@@ -73,7 +73,7 @@ const epics::pvData::int16 PVA_DEFAULT_PRIORITY = 0;
 const epics::pvData::uint32 MAX_CHANNEL_NAME_LENGTH = 500;
 
 /** Invalid data type. */
-const epics::pvData::int16 INVALID_DATA_TYPE = 0xFFFF;
+const epics::pvData::int16 INVALID_DATA_TYPE = -1;
 
 /** Invalid IOID. */
 const epics::pvData::int32 INVALID_IOID = 0;
